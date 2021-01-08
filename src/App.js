29632ff97c3fc959253664b495240c2b18bfa150
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-08 08:42:50 -0300

    components, props and propTypes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
 import './App.css';
+import React, { Component } from 'react';
+
+import Navbar from './components/Navbar';
 
 class App extends Component {
-  foo = () => 'Bar';
-
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = false;
-
-    // if(loading) {
-    //   return <h4>Loading...</h4>
-    // }
-
     return (
       <div className="app">
-        {loading ? <h4>Loading...</h4> 
-          : <h1>Hello {showName && name }</h1>}
+        <Navbar />
       </div>
     );
   }
