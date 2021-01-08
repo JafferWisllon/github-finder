ba14de1b0ef3bb2a5eb15c0b21392032e5835c48
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-08 09:04:12 -0300

    lists and passing state with props

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      id: '1',
-      login: 'mojombo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo'
-    }
-  }
-  
   render() {
-    const { avatar_url, login, html_url } = this.state;
+    const { user } = this.props;
+    
     return (
       <div className="card text-center">
-        <img src={avatar_url} alt="" className="round-img" style={{
+        <img src={user.avatar_url} alt="" className="round-img" style={{
           width: '60px'
         }}/>
-        <h3>{login}</h3>
+        <h3>{user.login}</h3>
         
         <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+          <a href={user.html_url} className="btn btn-dark btn-sm my-1">More</a>
         </div>
       </div>
     )
