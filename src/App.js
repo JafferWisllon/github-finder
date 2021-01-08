3a59722fe1a2c63278a7fcea9d2d1eff251aec35
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-08 08:23:32 -0300

    jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  foo = () => 'Bar';
+
   render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = false;
+
+    // if(loading) {
+    //   return <h4>Loading...</h4>
+    // }
+
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className="app">
+        {loading ? <h4>Loading...</h4> 
+          : <h1>Hello {showName && name }</h1>}
       </div>
     );
   }
