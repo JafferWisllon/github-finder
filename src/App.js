239311e117ef79662c5d7e18990f9c77a15f1154
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-09 14:29:45 -0300

    alert state and component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 import axios from 'axios';
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null,
   }
 
   // Search Users
@@ -33,16 +35,29 @@ class App extends React.Component {
     })
   }
 
+  setAlert = (msg, type) => {
+    this.setState({
+      alert: {
+        msg,
+        type
+      }
+    })
+
+    setTimeout(() => this.setState({ alert: null}), 5000);
+  }
+
   render() {
-    const { loading, users } = this.state
+    const { loading, users, alert } = this.state
     return (
       <div className="app">
         <Navbar />
         <div className="container">
+          <Alert alert={alert}/>
           <Search 
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users}/>
         </div>
